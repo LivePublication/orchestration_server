7f2e3de6442745a517744bbeb35a7fd72e89f76d
Nelis Drost <nelis.drost@gmail.com>
2023-09-01 15:36:42 +1200

    Basic flask app with example html GET endpoint and example POST endpoint

---
+++ app.py
@@ -0,0 +1,35 @@
+import flask
+from werkzeug.middleware.proxy_fix import ProxyFix
+import logging
+
+app = flask.Flask(__name__)
+# IMPORTANT: the commented code below is required if we're running behind a proxy (e.g.: nginx) - it should not be
+# uncommented otherwise
+# app.wsgi_app = ProxyFix(
+#     app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
+# )
+
+
+# Tie in gunicorn logger
+gunicorn_logger = logging.getLogger('gunicorn.error')
+app.logger.handlers.extend(gunicorn_logger.handlers)
+app.logger.setLevel(logging.INFO)
+info = app.logger.info
+
+
+@app.route('/')
+def index():
+    return flask.render_template('index.html', title='Title', text='This is the home page')
+
+
+# Example POST REST endpoint
+@app.route('/test/', methods=['POST'])
+def test():
+    # Do something with query params
+    info(flask.request.args.to_dict())
+
+    # Do something with the body if it's json
+    if flask.request.is_json:
+        info(flask.request.json)
+
+    return flask.jsonify(flask.request.args)
