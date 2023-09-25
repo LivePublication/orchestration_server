04295b480cce00b6e84e2e1f55a64c86ba2d1728
Augustus Ellerm <ellermaugustus@gmail.com>
2023-09-26 09:59:30 +1300

    first integration pass for orchestration_logic into orchestration_server. Basic implementation for testing

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 import flask
-from werkzeug.middleware.proxy_fix import ProxyFix
 import logging
+
+from werkzeug.middleware.proxy_fix import ProxyFix
+from pathlib import Path
+from orchestration_logic.LidFlow import LidFlow
+from orchestration_logic.orchestration_crate import Orchestration_crate
+from orchestration_logic.orchestration_types import OrchestrationData
+from globus_sdk import LocalGlobusConnectPersonal
+from flow_config import *
+
 
 app = flask.Flask(__name__)
 # IMPORTANT: the commented code below is required if we're running behind a proxy (e.g.: nginx) - it should not be
@@ -33,8 +41,44 @@ def static_favicon(filespec):
 
 
 # Example POST REST endpoint
-@app.route('/test/', methods=['POST'])
-def test():
+@app.route('/LiD/', methods=['POST'])
+def execute_LiD_flow():
+
+    # Grab endpoint details for transfers
+    local_gcp = LocalGlobusConnectPersonal()
+    ep_id = local_gcp.endpoint_id
+
+    # Using locally defined config, could be passed in as a parameter in json body
+    config = {
+        "endpoints": endpoints,
+        "data_paths": data_paths,
+        "intermediate_paths": intermediate_paths,
+        "LP_configuration": LP_configuration,
+        "run_label": run_label,
+        "run_tags": run_tags
+    }
+    # Edit config to include dymamic gcp endpoint
+    config["LP_configuration"]["orchestration_node"] = ep_id
+
+    # Execute the gladier flow
+    lid_flow = LidFlow(endpoints=endpoints, 
+                       data_paths=data_paths, 
+                       intermediate_paths=intermediate_paths, 
+                       LP_configuration=LP_configuration, 
+                       run_label=run_label, 
+                       run_tags=run_tags)
+    lid_flow.run()
+    lid_flow.monitor_run()
+    lid_flow.monitor_transfer()
+    orchestration_data: OrchestrationData = lid_flow.get_data()
+    orchestration_crate = Orchestration_crate(lid_flow, 
+                                              orchestration_data, 
+                                              (Path.cwd() / "orchestration_crate"), 
+                                              config['run_label'], 
+                                              config['run_tags'])
+    orchestration_crate.build_crate()
+    orchestration_crate.clean_up()
+
     # Do something with query params
     info(flask.request.args.to_dict())
 
---
+++ flow_config.py
@@ -0,0 +1,36 @@
+# Configuration for LidFlow as top level attributes (we could easily move this to a config file)
+# Customizable attributes for re-execution by readers? # TODO
+endpoints = {
+    "FT_UUID": "5612672e-1ead-11ee-abf1-63e0d97254cd",
+    "ST_UUID": "105a24f4-2a94-11ee-8801-056a4e394379",
+    "LD_UUID": "21968ff8-29c4-11ee-87ff-056a4e394379",
+    "DS_UUID": "d6215ec8-244a-11ee-80c1-a3018385fcef"
+}
+
+# Data paths for the actual test and validation data
+data_paths = {
+    "validation_path": "/home/ubuntu/LiD_Datastore/validation.txt",
+    "data_path": "/home/ubuntu/LiD_Datastore/input_data.txt"
+}
+
+# Intermediate paths for data transfer between LPAPs
+intermediate_paths = {
+    "validation_dest_path": "/home/ubuntu/statistics_lpap/input/validation.txt",
+    "DS_FT_dest": "/home/ubuntu/fastText_lpap/input/input_data.txt",
+    "DS_LD_dest": "/home/ubuntu/langdetect_lpap/input/input_data.txt",
+    "FT_ST_dest": "/home/ubuntu/statistics_lpap/input/fastText_predictions.txt",
+    "LD_ST_dest": "/home/ubuntu/statistics_lpap/input/langdetect_predictions.txt",
+    "FT_output_path": "/home/ubuntu/fastText_lpap/output/fastText_predictions.txt",
+    "LD_output_path": "/home/ubuntu/langdetect_lpap/output/langdetect_predictions.txt"
+}
+
+# LivePub name/subcrate path & orchestration node UUID
+# TODO change article_name to subcrate_path -> will need to change in LPAPs 
+LP_configuration = {
+    "orchestration_node": "None",
+    "article_name": "/Users/eller/Projects/orchestration_server/sub_crates"
+}
+
+# Run label and tags for the flow
+run_label = "LiDFlow run"
+run_tags = ["LID", "Orchestration", "Test"]
